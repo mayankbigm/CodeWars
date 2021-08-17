07f27574f10e6eacbc6fd51170ad6df8e7c0ecae
Mayank Gupta <mgupta@homesite.com>
2021-08-17 00:01:08 -0400

    HighestAndLowestNumberInString

---
+++ CodeWarsKataTrainer/Training/HighestLowestNumbersInString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Training
+{
+    /// <summary>
+    ///  given a string of space separated numbers, and have to return the highest and lowest number.
+    /// Example:
+    /// Kata.HighAndLow("1 2 3 4 5");  // return "5 1"
+    /// Kata.HighAndLow("1 2 -3 4 5"); // return "5 -3"
+    /// Kata.HighAndLow("1 9 3 4 -5"); // return "9 -5"
+    /// </summary>
+    public static class HighestLowestNumbersInString
+    {
+        public static string HighAndLow(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                throw new ArgumentNullException(nameof(numbers));
+            }
+
+            var digits = numbers.Split(" ").Select(int.Parse).ToList();
+            return $"{digits.Max()} {digits.Min()}";
+        }
+    }
+}
--- CodeWarsKataTrainer/Training/Program.cs
+++ CodeWarsKataTrainer/Training/Program.cs
@@ -13,7 +13,11 @@ namespace Training
             //FormatPhoneNumber.CreatePhoneNumber(new int[]{1,2,3,4,5,6,7,8,9,0});
 
             // ListFilterer
-            ListFilterer.GetIntegersFromList(new List<object>(){1, 2, "a", "b"});
+            //ListFilterer.GetIntegersFromList(new List<object>(){1, 2, "a", "b"});
+
+            //HighAndLowNumbers
+            var result = HighestLowestNumbersInString.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
+            Console.WriteLine(result);
 
             Console.WriteLine("End!");
         }
