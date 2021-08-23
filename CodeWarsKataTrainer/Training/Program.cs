ca01a062f8c7ddaaa76389fdb61a1efecaa49478
Mayank Gupta <mgupta@homesite.com>
2021-08-22 23:52:34 -0400

    Simple Pig Latin

--- CodeWarsKataTrainer/Training/Program.cs
+++ CodeWarsKataTrainer/Training/Program.cs
@@ -5,22 +5,25 @@ namespace Training
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Begin!");
 
-            // CreatePhoneNumber
-            //FormatPhoneNumber.CreatePhoneNumber(new int[]{1,2,3,4,5,6,7,8,9,0});
+            // Simple Pig Latin
+            var result = SimplePigLatin.PigIt("Hello world !");
+            Console.WriteLine(result);
 
-            // ListFilterer
-            //ListFilterer.GetIntegersFromList(new List<object>(){1, 2, "a", "b"});
+            //LineTickets
+            //var result = LineTickets.Tickets(new int[] { 25, 25, 50, 100, 25, 50, 25, 100, 25, 50, 25, 100, 25, 50, 25, 100, 50, 50 });
 
             //HighAndLowNumbers
             //var result = HighestLowestNumbersInString.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
 
-            //LineTickets
-            var result = LineTickets.Tickets(new int[] { 25, 25, 50, 100, 25, 50, 25, 100, 25, 50, 25, 100, 25, 50, 25, 100, 50, 50 });
-            Console.WriteLine(result);
+            // ListFilterer
+            //ListFilterer.GetIntegersFromList(new List<object>(){1, 2, "a", "b"});
+
+            // CreatePhoneNumber
+            //FormatPhoneNumber.CreatePhoneNumber(new int[]{1,2,3,4,5,6,7,8,9,0});
 
             Console.WriteLine("End!");
         }
---
+++ CodeWarsKataTrainer/Training/SimplePigLatin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Training
+{
+    public static class SimplePigLatin
+    {
+        public static string PigIt(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                throw new ArgumentNullException(s);
+            }
+
+            Console.WriteLine(s);
+
+            var result = s.Split(" ")
+                .Select(x => char.IsPunctuation(x[0]) ? x : $"{x.Substring(1)}{x.Substring(0, 1)}ay")
+                .ToArray();
+
+            return string.Join(" ", result);
+        }
+    }
+}
